c082ce623f8a03983b4824dbc213c3d5694e2c54
ahmetoztrk <ahmetoztrk1709@gmail.com>
2023-04-02 01:11:02 +0300

    added delete option

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ encoded: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'));
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
 app.get('/', async (req, res) => {
@@ -48,7 +52,7 @@ app.get('/photos/:id', async (req, res) => {
 });
 
 app.get('/photos/edit/:id', async (req, res) => {
-  const photo = await Photo.findOne({_id: req.params.id});
+  const photo = await Photo.findOne({ _id: req.params.id });
   res.render('edit', {
     photo,
   });
@@ -80,6 +84,14 @@ app.put('/photos/:id', async (req, res) => {
   photo.save();
 
   res.redirect(`/photos/${req.params.id}`);
+});
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
 });
 
 const port = 3000;
